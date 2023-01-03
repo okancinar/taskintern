d31287b8314db19c74fc6c9f1e1647fb5dfa47c4
Okan Cinar <okancinar10189@gmail.com>
2023-01-03 16:23:45 +0300

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM openjdk:19
 COPY target/spring-boot-rest-api-0.0.1-SNAPSHOT.jar spring-boot-rest-api-0.0.1-SNAPSHOT.jar
-
 ENTRYPOINT ["java","-jar","/spring-boot-rest-api-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/cinar/okan/service/UserService.java
+++ src/main/java/com/cinar/okan/service/UserService.java
@@ -4,7 +4,7 @@ import com.cinar.okan.entity.User;
 import java.util.List;
 
 public interface UserService {
-    User createUser(User user); // service katmanına bu metodu yazarken public abstract yazmaya gerek yok ama bu kısmı anlamadım.
+    User createUser(User user); // service katmanına bu metodu yazarken public abstract yazmaya gerek yok ama bu kısmı tam anlamadım.
     List<User> getUsers();
     User getUser(Long id);
 
