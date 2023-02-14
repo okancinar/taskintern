9ea5f6410a7f0491fb6cbf6d3a7a12c6c7ccc2da
ocinar <okancinar10189@gmail.com>
2023-02-14 13:12:50 +0300

    dockor-compose dosyası eklendi.

--- src/main/java/com/cinar/okan/entity/User.java
+++ src/main/java/com/cinar/okan/entity/User.java
@@ -14,6 +14,7 @@ public class User {
     //primary key özelliğinin artırım stratejisini belirleriz
     @SequenceGenerator(name = "user_seq_gen",sequenceName = "user_gen", initialValue = 100,allocationSize = 1)
     @GeneratedValue(strategy= GenerationType.SEQUENCE,generator = "user_seq_gen")
+    //Burayı tam anlamadım 
     @Column(name = "ID")
     private Long id;
     @Column(name = "FIRST NAME" , length = 100)
